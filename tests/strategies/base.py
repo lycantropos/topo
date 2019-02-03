df55e63dd307daacb92c6d06d07447627f0861f8
Ibrakov Azat <azatibrakov@gmail.com>
2019-02-03 10:37:18 +0300

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -1,12 +1,12 @@
 from typing import (Iterable,
-                    Optional,
-                    Set)
+                    Optional)
 
 from hypothesis import strategies
 from hypothesis.searchstrategy import SearchStrategy
 
 from tests.utils import Domain
 from topo.base import (EMPTY_SET,
+                       Set,
                        Union)
 from .literals.factories import to_homogeneous_tuples
 
@@ -14,9 +14,12 @@ from .literals.factories import to_homogeneous_tuples
 def to_unions(elements: SearchStrategy[Set[Domain]],
               *,
               min_size: int = 0,
-              max_size: Optional[int] = None) -> SearchStrategy[Union[Domain]]:
-    def union_from_sets(sets: Iterable[Set]) -> Union:
-        return Union(*sets)
+              max_size: Optional[int] = None) -> SearchStrategy[Set[Domain]]:
+    def union_from_sets(sets: Iterable[Set]) -> Set:
+        union = Union(*sets)
+        if isinstance(union, Union) and len(union.subsets) == 1:
+            return list(union.subsets)[0]
+        return union
 
     return (to_homogeneous_tuples(elements,
                                   min_size=min_size,
